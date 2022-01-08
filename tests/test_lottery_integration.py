a8b4ccdaecc176a1c3646b4f9580771c86c29712
TImofey Kabakov <br220701@gmail.com>
2022-01-08 15:42:41 +0300

    1

---
+++ tests/test_lottery_integration.py
@@ -0,0 +1,25 @@
+from scripts.helpful_scripts import (
+    LOCAL_BLOCKCHAIN_ENVIROMENTS,
+    get_account,
+    get_contract,
+    fund_with_link,
+)
+from scripts.deploy_lottery import deploy_lottery
+import pytest
+from brownie import network
+import time
+
+
+def test_can_pick_winner():
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIROMENTS:
+        pytest.skip()
+    lottery = deploy_lottery()
+    account = get_account()
+    lottery.startLottery({"from": account})
+    lottery.enter({"from": account, "value": lottery.getEntranceFee()})
+    lottery.enter({"from": account, "value": lottery.getEntranceFee()})
+    fund_with_link(lottery)
+    lottery.endLottery({"from": account})
+    time.sleep(60)
+    assert lottery.recentWinner() == account
+    assert lottery.balance == 0
